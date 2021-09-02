fe05b7d31f903f973c4ad1b89f0c8016176bee51
Arnar <arnare20@ru.is>
2021-09-02 22:13:09 +0000

    finished

--- sequence.py
+++ sequence.py
@@ -10,12 +10,17 @@ b = 1
 c = 2
 
 # a for loop run for n times
-# calculating a summing the sequence formula
-# finally printing out the sum
-print(b)
-for i in range(1,n+1):
-    d = a+b+c
-    a = b
-    b = c
-    c = d
-    print(d)
+# calculating the sequence formula
+# finally printing out the sums
+
+if n >= b:
+    print(b)
+if n >= c:
+    print(c)
+if n > c:
+    for i in range(0,n-2):
+        d = a+b+c
+        a = b
+        b = c
+        c = d
+        print(d)
