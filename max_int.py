6300cba2a88af27b3599e19bb7e9d86e0ecc5980
Arnar <arnare20@ru.is>
2021-09-02 21:07:28 +0000

    finised

--- max_int.py
+++ max_int.py
@@ -7,12 +7,10 @@ max_int = 0
 
 # is it negative?
 while num_int >= 0:
+    # if not ask for another number
     num_int = int(input("Input a number: "))
     if num_int >= max_int:
         max_int = num_int
-
-# if not ask for another number
-
 # if it is negative, print the 
 # highest number the user has inputed
 print("The maximum is", max_int)    # Do not change this line
