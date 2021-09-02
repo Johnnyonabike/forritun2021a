a71bacec876ef8cffc580fe43811234d4aa107ab
Arnar <arnare20@ru.is>
2021-09-02 20:52:11 +0000

    started writing code

--- max_int.py
+++ max_int.py
@@ -1,6 +1,16 @@
 # algorithm for max_int
+
+
 # get number as input
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 0
+
 # is it negative?
+if num_int < 0:
+    print(max_int)
+
 # if not ask for another number
+
 # if it is negative, print the 
 # highest number the user has inputed
+print("The maximum is", max_int)    # Do not change this line
